1fb38852ed80b5f7b6e1021e8707a30f5bde501e
Gustavo Bordoni <bordoni.dev@gmail.com>
2025-04-04 17:24:29 -0400

    Add link back to github folder.

--- button-group-iac/src/index.js
+++ button-group-iac/src/index.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Header from './components/Header';
 import AttendeeFields from './components/AttendeeFields';
 import './index.css';
-
-const BASE_PATH = '/concepts';
 
 function App() {
   return (
     <div className="App">
-      <header className="concept-header">
-        <a href={`${BASE_PATH}/`} className="home-link">
-          ← Back to Concepts
-        </a>
-        <h1>Bordoni Concepts</h1>
-      </header>
+      <Header />
       <AttendeeFields />
     </div>
   );
